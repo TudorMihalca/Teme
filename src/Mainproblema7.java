12cbbf6e2d4ed5c96cfa303afb16a88167ed26c0
Tudor Mihalca <tudor_1994@yahoo.com>
2018-05-12 20:41:21 +0300

    problema 7

---
+++ src/Mainproblema7.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Mainproblema7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double numar;
+        double tva;
+        System.out.println("Numarul este:  ");
+        numar = scanner.nextInt();
+        tva = numar * 19/119;
+        System.out.println("Tva-ul este: " +tva);
+
+
+
+
+
+    }
+}
