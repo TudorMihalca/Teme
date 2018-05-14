ce0180e00b60ef9272b992cab17bb4aca4f6f74b
Tudor Mihalca <tudor_1994@yahoo.com>
2018-05-14 16:44:33 +0300

    problema 12

---
+++ src/MainProblema12.java
@@ -0,0 +1,14 @@
+public class MainProblema12 {
+    public static void main(String[] args) {
+
+        long n=20;
+        long suma=0;
+
+        suma=(n*(n+1)/2);
+
+        System.out.println("suma primelor" + n + "numere este" + suma);
+
+
+
+    }
+}
